b1140bf844d88b045a46b7904f539b5dc5cf864d
Franziskus Kiefer <franziskuskiefer@gmail.com>
2021-06-10 09:44:35 +0200

    finish openmls crypto api

--- src/openmls_crypto.rs
+++ src/openmls_crypto.rs
@@ -1,7 +1,20 @@
+//! # OpenMLS Crypto API
+//!
+//! This module implements the [`openmls_crypto`] API for HPKE.
+
 use std::convert::TryInto;
 
-use crypto_algorithms::{AeadType, AsymmetricKeyType, KdfType, KemKeyType};
-use openmls_crypto::{hpke::HpkeSeal, keys::PublicKey, secret::Secret};
+use crypto_algorithms::{AeadType, AsymmetricKeyType, HashType, KdfType, KemKeyType, KemType};
+use evercrypt::{
+    openmls_crypto::Evercrypt,
+    sqlite_key_store::{KeyStoreTrait, PrivateKey},
+};
+use openmls_crypto::{
+    hash::Hash,
+    hpke::{HpkeDerive, HpkeOpen, HpkeSeal},
+    keys::PublicKey,
+    secret::Secret,
+};
 
 use crate::{kem, Hpke, Mode as HpkeMode};
 
@@ -23,8 +36,8 @@ fn evercrypt_kem_type(
 }
 
 impl HpkeSeal for Hpke {
-    type KeyStoreType;
-    type KeyStoreIndex;
+    type KeyStoreType = evercrypt::sqlite_key_store::KeyStore;
+    type KeyStoreIndex = evercrypt::sqlite_key_store::KeyStoreId;
 
     fn hpke_seal(
         key_store: &Self::KeyStoreType,
@@ -35,12 +48,11 @@ impl HpkeSeal for Hpke {
         aad: &[u8],
         payload: &[u8],
     ) -> Result<(Vec<u8>, Vec<u8>), openmls_crypto::errors::Error> {
-        let (pk_r, _): (PublicKey, _) = key_store.internal_read(key_id)?;
+        let (pk_r, _): (PublicKey, _) = key_store.unsafe_read(key_id)?;
         Hpke::hpke_seal_to_pk(kdf, aead, &pk_r, info, aad, payload)
     }
 
     fn hpke_seal_to_pk(
-        key_store: &Self::KeyStoreType,
         kdf: KdfType,
         aead: AeadType,
         key: &PublicKey,
@@ -72,7 +84,7 @@ impl HpkeSeal for Hpke {
         aad: &[u8],
         secret_id: &Self::KeyStoreIndex,
     ) -> Result<(Vec<u8>, Vec<u8>), openmls_crypto::errors::Error> {
-        let (pk_r, _): (PublicKey, _) = key_store.internal_read(key_id)?;
+        let (pk_r, _): (PublicKey, _) = key_store.unsafe_read(key_id)?;
         Self::hpke_seal_secret_to_pk(key_store, kdf, aead, &pk_r, info, aad, secret_id)
     }
 
@@ -86,7 +98,7 @@ impl HpkeSeal for Hpke {
         secret_id: &Self::KeyStoreIndex,
     ) -> Result<(Vec<u8>, Vec<u8>), openmls_crypto::errors::Error> {
         let kem = evercrypt_kem_type(key.key_type())?;
-        let (secret, _): (Secret, _) = key_store.internal_read(secret_id)?;
+        let (secret, _): (Secret, _) = key_store.unsafe_read(secret_id)?;
         let hpke = Hpke::new(
             HpkeMode::Base,
             (kem as u16).try_into().unwrap(),
@@ -110,101 +122,87 @@ impl HpkeSeal for Hpke {
     }
 }
 
-// impl HpkeOpen for KeyStore {
-//     fn hpke_open_with_sk(
-//         &self,
-//         kdf: HpkeKdfType,
-//         aead: AeadType,
-//         key_id: &impl KeyStoreId,
-//         cipher_text: &[u8],
-//         kem_out: &KemOutput,
-//         info: &[u8],
-//         aad: &[u8],
-//     ) -> Result<Plaintext, openmls_crypto::errors::Error> {
-//         let (sk_r, _status): (PrivateKey, Status) = self.internal_read(key_id)?;
-//         let kem = evercrypt_kem_type(sk_r.key_type())?;
-//         let hpke = Hpke::new(
-//             hpke::Mode::Base,
-//             (kem as u16).try_into().unwrap(),
-//             (kdf as u16).try_into().unwrap(),
-//             (aead as u16).try_into().unwrap(),
-//         );
-//         let ptxt = hpke
-//             .open(
-//                 kem_out.as_slice(),
-//                 &sk_r.as_slice().into(),
-//                 info,
-//                 aad,
-//                 cipher_text,
-//                 None,
-//                 None,
-//                 None,
-//             )
-//             .map_err(|e| Error::CryptoLibError(format!("HPKE Open error: {:?}", e)))?;
-//         Ok(Plaintext::new(ptxt))
-//     }
-// }
+impl HpkeOpen for Hpke {
+    type KeyStoreType = evercrypt::sqlite_key_store::KeyStore;
+    type KeyStoreIndex = evercrypt::sqlite_key_store::KeyStoreId;
 
-// impl HpkeDerive for KeyStore {
-//     fn derive_key_pair(
-//         &self,
-//         kem: HpkeKemType,
-//         kdf: HpkeKdfType,
-//         aead: AeadType,
-//         ikm_id: &impl KeyStoreId,
-//         private_key_id: &impl KeyStoreId,
-//         label: &[u8],
-//     ) -> Result<PublicKey, openmls_crypto::errors::Error> {
-//         let (ikm, _status): (Secret, Status) = self.internal_read(ikm_id)?;
-//         let hpke = Hpke::new(
-//             hpke::Mode::Base,
-//             (kem as u16).try_into().unwrap(),
-//             (kdf as u16).try_into().unwrap(),
-//             (aead as u16).try_into().unwrap(),
-//         );
-//         let key_pair = hpke
-//             .derive_key_pair(ikm.as_slice())
-//             .map_err(|e| Error::CryptoLibError(format!("HPKE Derive key pair error: {:?}", e)))?;
-//         let (private_key, public_key) = key_pair.into_keys();
-//         let key_type = AsymmetricKeyType::try_from(kem)?;
-//         let public_key = PublicKey::from(key_type, public_key.as_slice(), label);
-//         let private_key = PrivateKey::from(key_type, private_key.as_slice(), label, &public_key);
-//         self.store(private_key_id, &private_key)?;
-//         Ok(public_key)
-//     }
-// }
+    fn hpke_open_with_sk(
+        key_store: &Self::KeyStoreType,
+        kdf: KdfType,
+        aead: AeadType,
+        key_id: &Self::KeyStoreIndex,
+        cipher_text: &[u8],
+        kem_enc: &[u8],
+        info: &[u8],
+        aad: &[u8],
+    ) -> Result<Vec<u8>, openmls_crypto::errors::Error> {
+        let (sk_r, _): (PrivateKey, _) = key_store.unsafe_read(key_id)?;
+        let kem = evercrypt_kem_type(sk_r.key_type())?;
+        let hpke = Hpke::new(
+            HpkeMode::Base,
+            (kem as u16).try_into().unwrap(),
+            (kdf as u16).try_into().unwrap(),
+            (aead as u16).try_into().unwrap(),
+        );
+        let ptxt = hpke
+            .open(
+                kem_enc,
+                &sk_r.as_slice().into(),
+                info,
+                aad,
+                cipher_text,
+                None,
+                None,
+                None,
+            )
+            .map_err(|e| {
+                openmls_crypto::errors::Error::CryptoLibError(format!("HPKE Open error: {:?}", e))
+            })?;
+        Ok(ptxt)
+    }
+}
 
-// // Generate KEM key pair and use it in HPKE.
-// let (pk, sk_id) = ks
-//     .new_key_pair(
-//         AsymmetricKeyType::KemKey(KemKeyType::X25519),
-//         Status::Hidden,
-//         b"hidden x25519 key pair",
-//     )
-//     .expect("Error generating x25519 key pair");
-// let err: Result<PrivateKey> = ks.read(&sk_id);
-// assert_eq!(err.err(), Some(Error::ForbiddenExtraction));
+impl HpkeDerive for Hpke {
+    type KeyStoreType = evercrypt::sqlite_key_store::KeyStore;
+    type KeyStoreIndex = evercrypt::sqlite_key_store::KeyStoreId;
 
-// let (ct, enc) = ks
-//     .hpke_seal_to_pk(
-//         KdfType::HkdfSha256,
-//         AeadType::Aes128Gcm,
-//         &pk,
-//         b"info string",
-//         b"test aad",
-//         b"HPKE test payload",
-//     )
-//     .expect("Error sealing to PK");
+    fn derive_key_pair(
+        key_store: &Self::KeyStoreType,
+        kem: KemType,
+        kdf: KdfType,
+        aead: AeadType,
+        ikm_id: &Self::KeyStoreIndex,
+        label: &[u8],
+    ) -> Result<(PublicKey, Self::KeyStoreIndex), openmls_crypto::errors::Error> {
+        let (ikm, _): (Secret, _) = key_store.unsafe_read(ikm_id)?;
+        let hpke = Hpke::new(
+            HpkeMode::Base,
+            (kem as u16).try_into().unwrap(),
+            (kdf as u16).try_into().unwrap(),
+            (aead as u16).try_into().unwrap(),
+        );
+        let key_pair = hpke.derive_key_pair(ikm.as_slice()).map_err(|e| {
+            openmls_crypto::errors::Error::CryptoLibError(format!(
+                "HPKE Derive key pair error: {:?}",
+                e
+            ))
+        })?;
+        let (private_key, public_key) = key_pair.into_keys();
+        let key_type = AsymmetricKeyType::from(kem);
+        let public_key = PublicKey::from(key_type, public_key.as_slice(), label);
+        let private_key = PrivateKey::from(key_type, private_key.as_slice(), label, &public_key);
 
-// let msg = ks
-//     .hpke_open_with_sk(
-//         KdfType::HkdfSha256,
-//         AeadType::Aes128Gcm,
-//         &sk_id,
-//         &ct,
-//         &enc,
-//         b"info string",
-//         b"test aad",
-//     )
-//     .expect("Error opening HPKE.");
-// assert_eq!(msg.as_slice(), b"HPKE test payload");
+        let mut sha256 = Evercrypt::hasher(HashType::Sha2_256)?;
+        sha256.update(label)?;
+        sha256.update(public_key.as_slice())?;
+        let mut private_key_id = [0u8; 32];
+        private_key_id.clone_from_slice(&sha256.finish()?);
+
+        key_store.store_with_status(
+            &private_key_id,
+            &private_key,
+            evercrypt::sqlite_key_store::Status::Hidden,
+        )?;
+        Ok((public_key, private_key_id))
+    }
+}
