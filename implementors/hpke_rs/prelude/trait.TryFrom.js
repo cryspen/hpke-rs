73bd951b48e4d3854fcd7e70dc44aca96e548411
franziskuskiefer <franziskuskiefer@users.noreply.github.com>
2022-01-17 13:54:59 +0000

    deploy: 0b469bc14b23f9b69a2c90e703eebe540d96b404

--- implementors/hpke_rs/prelude/trait.TryFrom.js
+++ implementors/hpke_rs/prelude/trait.TryFrom.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["hpke_rs"] = [{"text":"impl <a class=\"trait\" href=\"hpke_rs/prelude/trait.TryFrom.html\" title=\"trait hpke_rs::prelude::TryFrom\">TryFrom</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.57.0/std/primitive.u8.html\">u8</a>&gt; for <a class=\"enum\" href=\"hpke_rs/enum.Mode.html\" title=\"enum hpke_rs::Mode\">Mode</a>","synthetic":false,"types":["hpke_rs::Mode"]}];
+implementors["hpke_rs"] = [{"text":"impl <a class=\"trait\" href=\"hpke_rs/prelude/trait.TryFrom.html\" title=\"trait hpke_rs::prelude::TryFrom\">TryFrom</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.58.0/std/primitive.u8.html\">u8</a>&gt; for <a class=\"enum\" href=\"hpke_rs/enum.Mode.html\" title=\"enum hpke_rs::Mode\">Mode</a>","synthetic":false,"types":["hpke_rs::Mode"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
