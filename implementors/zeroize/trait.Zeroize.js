61c96044744425d0c4e21904e652a88d4d330de7
franziskuskiefer <franziskuskiefer@users.noreply.github.com>
2022-01-29 15:21:20 +0000

    deploy: bfe0a88fdf4bcf9071118bd8d90e3a8bcb144498

---
+++ implementors/zeroize/trait.Zeroize.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["hpke_rs"] = [{"text":"impl <a class=\"trait\" href=\"https://docs.rs/zeroize/1.4.3/zeroize/trait.Zeroize.html\" title=\"trait zeroize::Zeroize\">Zeroize</a> for <a class=\"struct\" href=\"hpke_rs/struct.HpkePrivateKey.html\" title=\"struct hpke_rs::HpkePrivateKey\">HpkePrivateKey</a>","synthetic":false,"types":["hpke_rs::HpkePrivateKey"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
