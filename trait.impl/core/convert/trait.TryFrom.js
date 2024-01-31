cc71d95d540281699496e73d7f961e8ca12c138a
franziskuskiefer <franziskuskiefer@users.noreply.github.com>
2024-01-31 08:55:40 +0000

    deploy: 7c486f19188013f2546e185b9759918df6e02901

---
+++ trait.impl/core/convert/trait.TryFrom.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"hpke_rs":[["impl <a class=\"trait\" href=\"hpke_rs/prelude/trait.TryFrom.html\" title=\"trait hpke_rs::prelude::TryFrom\">TryFrom</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.75.0/std/primitive.u8.html\">u8</a>&gt; for <a class=\"enum\" href=\"hpke_rs/enum.Mode.html\" title=\"enum hpke_rs::Mode\">Mode</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
