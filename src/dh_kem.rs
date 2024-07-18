4f6d3cb66945ced087ba2359a436259a56c74d88
Jeff Gardner <202880+erskingardner@users.noreply.github.com>
2024-07-18 10:16:25 +0200

    Add changelog entry and remove duplication in KEM key gen

--- src/dh_kem.rs
+++ src/dh_kem.rs
@@ -66,7 +66,7 @@ pub(super) fn derive_key_pair<Crypto: HpkeCrypto>(
             &[],
             alg.private_key_len(),
         )?,
-        KemAlgorithm::DhKemP256 => {
+        KemAlgorithm::DhKemP256 | KemAlgorithm::DhKemK256 => {
             let mut ctr = 0u8;
             // Do rejection sampling trying to find a valid key.
             // It is expected that there aren't too many iteration and that
@@ -88,35 +88,7 @@ pub(super) fn derive_key_pair<Crypto: HpkeCrypto>(
                 if ctr == u8::MAX {
                     // If we get here we lost. This should never happen.
                     return Err(Error::CryptoLibraryError(
-                        "Unable to generate a valid P256 private key".to_string(),
-                    ));
-                }
-                ctr += 1;
-            }
-        }
-        KemAlgorithm::DhKemK256 => {
-            let mut ctr = 0u8;
-            // Do rejection sampling trying to find a valid key.
-            // It is expected that there aren't too many iteration and that
-            // the loop will always terminate.
-            loop {
-                let candidate = labeled_expand::<Crypto>(
-                    alg.into(),
-                    &dkp_prk,
-                    suite_id,
-                    "candidate",
-                    &ctr.to_be_bytes(),
-                    alg.private_key_len(),
-                );
-                if let Ok(sk) = &candidate {
-                    if let Ok(sk) = Crypto::kem_validate_sk(alg, sk) {
-                        break sk;
-                    }
-                }
-                if ctr == u8::MAX {
-                    // If we get here we lost. This should never happen.
-                    return Err(Error::CryptoLibraryError(
-                        "Unable to generate a valid K256 private key".to_string(),
+                        "Unable to generate a valid private key".to_string(),
                     ));
                 }
                 ctr += 1;
