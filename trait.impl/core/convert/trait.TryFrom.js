e26dd726a3be17464bbf90b064fa001eeeb5936e
franziskuskiefer <franziskuskiefer@users.noreply.github.com>
2026-04-07 13:03:06 +0000

    deploy: fd9734e0d5b197cd9644bfdc69bc8fd3ccc3d7db

--- trait.impl/core/convert/trait.TryFrom.js
+++ trait.impl/core/convert/trait.TryFrom.js
@@ -1,5 +1,5 @@
 (function() {
-    const implementors = Object.fromEntries([["hpke_rs",[["impl <a class=\"trait\" href=\"hpke_rs/prelude/trait.TryFrom.html\" title=\"trait hpke_rs::prelude::TryFrom\">TryFrom</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.94.0/std/primitive.u8.html\">u8</a>&gt; for <a class=\"enum\" href=\"hpke_rs/enum.Mode.html\" title=\"enum hpke_rs::Mode\">Mode</a>",0]]]]);
+    const implementors = Object.fromEntries([["hpke_rs",[["impl <a class=\"trait\" href=\"hpke_rs/prelude/trait.TryFrom.html\" title=\"trait hpke_rs::prelude::TryFrom\">TryFrom</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.94.1/std/primitive.u8.html\">u8</a>&gt; for <a class=\"enum\" href=\"hpke_rs/enum.Mode.html\" title=\"enum hpke_rs::Mode\">Mode</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
--- trait.impl/core/error/trait.Error.js
+++ trait.impl/core/error/trait.Error.js
@@ -1,5 +1,5 @@
 (function() {
-    const implementors = Object.fromEntries([["hpke_rs",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.94.0/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"hpke_rs/enum.HpkeError.html\" title=\"enum hpke_rs::HpkeError\">HpkeError</a>",0]]]]);
+    const implementors = Object.fromEntries([["hpke_rs",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.94.1/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"hpke_rs/enum.HpkeError.html\" title=\"enum hpke_rs::HpkeError\">HpkeError</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
