bd8b52575fbb4c2d97529417524aa4e1166e7ccd
franziskuskiefer <franziskuskiefer@users.noreply.github.com>
2026-04-21 09:29:30 +0000

    deploy: ee8273987fd068cc91b1b2584f21314476a9596b

--- trait.impl/core/ops/drop/trait.Drop.js
+++ trait.impl/core/ops/drop/trait.Drop.js
@@ -1,5 +1,5 @@
 (function() {
-    const implementors = Object.fromEntries([["hpke_rs",[["impl&lt;Crypto: 'static + HpkeCrypto&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.94.1/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"hpke_rs/struct.Context.html\" title=\"struct hpke_rs::Context\">Context</a>&lt;Crypto&gt;",0]]]]);
+    const implementors = Object.fromEntries([["hpke_rs",[["impl&lt;Crypto: 'static + HpkeCrypto&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.95.0/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"hpke_rs/struct.Context.html\" title=\"struct hpke_rs::Context\">Context</a>&lt;Crypto&gt;",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
